04e62f7f413bb1f925c7fa4209608369c825866f
Nikas-void <dumanbek17@gmail.com>
2025-12-04 15:28:32 +0800

    first test navbar

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+export const CATEGORIES_SLUGS = [
+  "home-decoration",
+  "kitchen-accessories",
+  "laptops",
+  "mens-watches",
+  "mobile-accessories",
+  "smartphones",
+  "sunglasses",
+  "tablets",
+  "womens-watches",
+];
