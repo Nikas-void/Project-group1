04a14f12b5b5316eadd87350991e95c4727ba031
Todo0901 <todoomn0901@gmail.com>
2025-12-08 16:09:56 +0800

    product-dit

--- src/app/components/voidpage.tsx
+++ src/app/components/voidpage.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { CATEGORIES_SLUGS } from '@/constants'
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 
 export default function voidpage() {
   return (
-    <div>dssdsdsddsdssdcart</div>
+    <>
+    <div className='ml-10'>
+
+    </div>
+    </>
   )
 }
 
